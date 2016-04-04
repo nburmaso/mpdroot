7bd34aaa1a27c47ec565dc26fe1944d4e92d5de9
Sergei Merts <>
2016-04-04 12:35:07 +0300

    Script for drawing colored picture of energy distribution in EMC was updated.

--- macro/emc/EmcDataAnalysis.C
+++ macro/emc/EmcDataAnalysis.C
@@ -1,5 +1,6 @@
 
 #include "TH1F.h"
+#include "TGraph.h"
 #include "TClonesArray.h"
 #include "TChain.h"
 #include "TH2F.h"
@@ -7,7 +8,12 @@
 using namespace std;
 using namespace TMath;
 
-void EmcDataAnalysis() {
+void EmcDataAnalysis(Int_t opt = 1) {
+
+    // opt:
+    // 1 - bin length corresponds to energy 
+    // 2 - bin length and color corresponds to energy 
+    // 3 - bin color corresponds to energy 
 
     /* Load basic libraries */
     gROOT->LoadMacro("$VMCWORKDIR/macro/mpd/mpdloadlibs.C");
@@ -28,117 +34,161 @@ void EmcDataAnalysis() {
     cout << " Number of events in DST file = " << events << endl;
 
     MpdEmcGeoPar* fGeoPar = new MpdEmcGeoPar();
+    Float_t rMinEmc = fGeoPar->GetRmin() * 0.1;
+    Float_t rMaxEmc = fGeoPar->GetRmax() * 0.1;
+    Float_t deltaR = rMaxEmc - rMinEmc;
+    
+    Float_t rMinTpc = 0.0;
+    Float_t rMaxTpc = 140.0;
+    Float_t rMinTof = 0.0;
+    Float_t rMaxTof = 156.0;
+    
 
-    Int_t nSec = fGeoPar->GetNsec();
-    Int_t nMod = fGeoPar->GetNmod();
-    Int_t nSupMod = fGeoPar->GetNsupMod();
-    Int_t nRow = fGeoPar->GetNrows();
-    
-    Float_t rMin = fGeoPar->GetRmin() * 0.1;
-    Float_t rMax = fGeoPar->GetRmax() * 0.1;
-    Float_t l = rMax - rMin;
-    
-//    TFile outFile("emc_histos.root", "RECREATE");
-    
-    UInt_t nBins = 500;
-    TH1F* hEne = new TH1F("hEne", "hEne", nBins, 0.0, 0.5);
-    TH1F* hRow = new TH1F("hRow", "hRow", nRow, 0, nRow);
-    TH1F* hSec = new TH1F("hSec", "hSec", 2 * nSec, 0, 2 * nSec);
-    TH1F* hMod = new TH1F("hMod", "hMod", nMod, 0, nMod);
-    TH1F* hSupMod = new TH1F("hSupMod", "hSupMod", nSupMod, 0, nSupMod);
-    TH2F* hScan = new TH2F("hScan", "hScan", 100, -300, 300, 100, 0, 360);
-    TH2F* hScanCyl = new TH2F("hScanCyl", "Cylindrical coordinates", 180, 0, 360, 300, -300, 300);
-    
-    Int_t n2Dbins = 300;
-    
-    TH2F* hXY_tpc = new TH2F("hXY_tpc", "hXY_tpc", 600, -(rMax + 10), rMax + 10, 600, -(rMax + 10), rMax + 10);
-    TH2F* hXY_tof = new TH2F("hXY_tof", "hXY_tof", 600, -(rMax + 10), rMax + 10, 600, -(rMax + 10), rMax + 10);
-    TH2F* hXY_emc = new TH2F("hXY_emc", "hXY_emc", n2Dbins, -(rMax + 10), rMax + 10, n2Dbins, -(rMax + 10), rMax + 10);
-    
-    
+    const Int_t n2Dbins = 360;
+    Float_t eneArr[n2Dbins]; // array of energies in each angle bin of EMC
+
+    TGraph* tpcGr = new TGraph();
+    TGraph* tofGr = new TGraph();
 
     events = 1;
+
     for (UInt_t iEv = 0; iEv < events; iEv++) {
         dstTree->GetEntry(iEv);
 
         UInt_t fNhits = EmcHits->GetEntriesFast();
 
         cout << " Number of EMC hits = " << fNhits << endl;
-        Float_t maxE = 0.0;
-        
-        for (UInt_t iHit = 0; iHit < fNhits; ++iHit) {
-            MpdEmcHit* hit = (MpdEmcHit*) EmcHits->At(iHit);
-            Float_t E = hit->GetE();
-            if (E > maxE) maxE = E;
-        } // hits loop
-        
-        for (UInt_t iHit = 0; iHit < fNhits; ++iHit) {
-            MpdEmcHit* hit = (MpdEmcHit*) EmcHits->At(iHit);
-            Int_t sec = hit->GetSec();
-            Int_t row = hit->GetRow();
-            Int_t mod = hit->GetMod();
-            Int_t supMod = hit->GetSupMod();
-            Float_t E = hit->GetE();
-            Float_t phi = hit->GetPhiCenter();
-            Float_t z = hit->GetZcenter();
-            
-            Float_t rMaxHit = rMin + E / maxE * l;
 
-            hScan->Fill(z, phi, E);
-            hScanCyl->Fill(phi, z, E);
-            hEne->Fill(E);
-            hRow->Fill(row, E);
-            hSec->Fill(sec, E);
-            hMod->Fill(mod, E);
-            hSupMod->Fill(supMod, E);
-            
-            Float_t emcXmin = rMin * Cos(phi * DegToRad());
-            Float_t emcYmin = rMin * Sin(phi * DegToRad());
-            Float_t emcXmax = rMaxHit * Cos(phi * DegToRad());
-            Float_t emcYmax = rMaxHit * Sin(phi * DegToRad());
-            
-            Int_t n = 10;
-            Float_t deltaX = (emcXmax - emcXmin) / n;
-            Float_t deltaY = (emcYmax - emcYmin) / n;
-            
-            
-            for (Int_t i = 0; i < 10; ++i) {
-                hXY_emc->Fill(emcXmin + deltaX * i, emcYmin + deltaY * i);
-            }
+        for (UInt_t iHit = 0; iHit < fNhits; ++iHit) {
+            MpdEmcHit* hit = (MpdEmcHit*) EmcHits->At(iHit);
+            Int_t iCur = GetIdxByPhi(hit->GetPhiCenter() * DegToRad(), n2Dbins);
+            eneArr[iCur] += hit->GetE();
         } // hits loop
-        
+
         for (UInt_t iHitTpc = 0; iHitTpc < TpcHits->GetEntriesFast(); ++iHitTpc) {
             MpdTpcHit* hitTpc = (MpdTpcHit*) TpcHits->At(iHitTpc);
-            Int_t x = hitTpc->GetX();
-            Int_t y = hitTpc->GetY();
-            
-            hXY_tpc->Fill(x, y, maxE * 0.01);
+            tpcGr->SetPoint(iHitTpc, hitTpc->GetX(), hitTpc->GetY());
         } // hits loop
-        
+
         for (UInt_t iHitTof = 0; iHitTof < TofHits->GetEntriesFast(); ++iHitTof) {
             MpdTofHit* hitTof = (MpdTofHit*) TofHits->At(iHitTof);
-            Int_t x = hitTof->GetX();
-            Int_t y = hitTof->GetY();
-            
-            hXY_tof->Fill(x, y, maxE * 0.005);
+            tofGr->SetPoint(iHitTof, hitTof->GetX(), hitTof->GetY());
         } // hits loop
-    } // event loop
-    
-    TCanvas* c1 = new TCanvas("c", "c", 1000, 1000);
-    hXY_tpc->Draw("BOX");
-    hXY_tof->Draw("SAME BOX");
-    hXY_emc->Draw("SAME BOX");
 
-    
-//    hScanCyl->Draw("SAME LEGO2Z POL");
-    
-    TCanvas* c2 = new TCanvas("c1", "c1", 1000, 1000);
-    hScan->Draw("CONT");
-//    hScan->Write();
-//    hScanCyl->Write();
-//    hRow->Write();
-//    hSec->Write();
-//    hSupMod->Write();
-//    hMod->Write();
-//    hEne->Write();
+    } // event loop
+
+    Float_t maxE = 0.0;
+    for (Int_t i = 0; i < n2Dbins; ++i) { //search for maximum bin
+        Float_t E = eneArr[i];
+        if (E > maxE) maxE = E;
+    }
+
+    for (Int_t i = 0; i < n2Dbins; ++i) { //normilizing
+        eneArr[i] = eneArr[i] / maxE;
+    }
+
+    TLine * line[n2Dbins];
+    Float_t dPhi = TwoPi() / n2Dbins;
+    Int_t lineWidth = 12;
+
+    const Int_t Number = 4;
+    const Int_t nb = 100;
+    Int_t myPalette[nb];
+    Double_t R[Number] = {0.00, 0.00, 1.00, 1.00};
+    Double_t G[Number] = {0.00, 1.00, 0.65, 0.00};
+    Double_t B[Number] = {1.00, 0.00, 0.00, 0.00};
+    Double_t Length[Number] = {0.0, 0.33, 0.66, 1.0};
+
+    Int_t FI = TColor::CreateGradientColorTable(Number, Length, R, G, B, nb);
+    for (Int_t i = 0; i < nb; ++i) {
+        myPalette[i] = FI + i;
+    }
+
+    if (opt == 1) {
+        for (Int_t i = 0; i < n2Dbins; ++i) {
+            Float_t phi = i * dPhi;
+            Float_t x1 = rMinEmc * Cos(phi);
+            Float_t y1 = rMinEmc * Sin(phi);
+            Float_t rNew = rMinEmc + eneArr[i] * deltaR;
+            Float_t x2 = rNew * Cos(phi);
+            Float_t y2 = rNew * Sin(phi);
+            line[i] = new TLine(x1, y1, x2, y2);
+            line[i]->SetLineWidth(lineWidth);
+            line[i]->SetLineColor(38);
+        }
+    } else if (opt == 2) {
+        for (Int_t i = 0; i < n2Dbins; ++i) {
+            Float_t phi = i * dPhi;
+            Float_t x1 = rMinEmc * Cos(phi);
+            Float_t y1 = rMinEmc * Sin(phi);
+            Float_t rNew = rMinEmc + eneArr[i] * deltaR;
+            Float_t x2 = rNew * Cos(phi);
+            Float_t y2 = rNew * Sin(phi);
+            Int_t colorId = Int_t(eneArr[i] * (nb - 1));
+            line[i] = new TLine(x1, y1, x2, y2);
+            line[i]->SetLineWidth(lineWidth);
+            line[i]->SetLineColor(myPalette[colorId]);
+        }
+    } else if (opt == 3) {
+        for (Int_t i = 0; i < n2Dbins; ++i) {
+            Float_t phi = i * dPhi;
+            Float_t x1 = rMinEmc * Cos(phi);
+            Float_t y1 = rMinEmc * Sin(phi);
+            Float_t x2 = rMaxEmc * Cos(phi);
+            Float_t y2 = rMaxEmc * Sin(phi);
+            Int_t colorId = Int_t(eneArr[i] * (nb - 1));
+            line[i] = new TLine(x1, y1, x2, y2);
+            line[i]->SetLineWidth(lineWidth);
+            if (eneArr[i] < 0.05 * maxE)
+                line[i]->SetLineColor(38);
+            else
+                line[i]->SetLineColor(myPalette[colorId]);
+        }
+    }
+
+
+    Double_t bound = 230.0;
+    TCanvas* c1 = new TCanvas("c", "c", 1000, 1000);
+    tpcGr->SetMarkerStyle(20);
+    tpcGr->SetMarkerColor(32);
+    tpcGr->SetMarkerSize(0.4);
+    tpcGr->GetXaxis()->SetLimits(-bound, bound);
+    tpcGr->SetMaximum(bound);
+    tpcGr->SetMinimum(-bound);
+    tpcGr->Draw("PA");
+
+    tofGr->SetMarkerStyle(20);
+    tofGr->SetMarkerColor(46);
+    tofGr->SetMarkerSize(0.4);
+    tofGr->Draw("SAME P");
+
+
+    for (Int_t i = 0; i < n2Dbins - 1; ++i) {
+        if ((opt == 1 || opt == 2) && eneArr[i] < 0.05 * maxE) continue;
+        line[i]->Draw("same");
+    }
+    TArc* iEmc = new TArc(0.0, 0.0, rMinEmc);
+    TArc* oEmc = new TArc(0.0, 0.0, rMaxEmc);
+    TArc* iTpc = new TArc(0.0, 0.0, rMinTpc);
+    TArc* oTpc = new TArc(0.0, 0.0, rMaxTpc);
+    TArc* iTof = new TArc(0.0, 0.0, rMinTof);
+    TArc* oTof = new TArc(0.0, 0.0, rMaxTof);
+    iEmc->SetFillStyle(0);
+    oEmc->SetFillStyle(0);
+    iTpc->SetFillStyle(0);
+    oTpc->SetFillStyle(0);
+    iTof->SetFillStyle(0);
+    oTof->SetFillStyle(0);
+    iEmc->Draw("same");
+    oEmc->Draw("same");
+    iTpc->Draw("same");
+    oTpc->Draw("same");
+    iTof->Draw("same");
+    oTof->Draw("same");
+
+}
+
+Int_t GetIdxByPhi(Float_t phi, Int_t N) {
+    //phi in radian
+    return Int_t(phi / TwoPi() * N);
 }
