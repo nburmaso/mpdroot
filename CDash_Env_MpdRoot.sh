d9e1cbdc311bf945bfcd0f632c5dc8d735e8192c
Konstantin Gertsenberger <soul@se51-99.jinr.ru>
2018-06-14 17:25:13 +0300

    CDASH correction according the new MpdRoot structure

--- CDash_Env_MpdRoot.sh
+++ CDash_Env_MpdRoot.sh
@@ -7,22 +7,24 @@ else
 fi
 
 export SIMPATH=/opt/fairsoft/install
+export FAIRROOTPATH=/opt/fairroot/install
 export SOURCEDIR=$CDASHUSERPATH
 export BUILDDIR=$HOME/CDASH/mpdroot_build_$(hostname -s)
 export LINUX_FLAVOUR=$(uname -o)
-export FAIRSOFT_VERSION=may16
+export FAIRSOFT_VERSION=oct17p1
 
 if [ "$#" -gt "1" ]; then
 if [ "$#" -gt "2" ]; then
 export FAIRSOFT_VERSION=$3
 else
-export FAIRSOFT_VERSION=may16
+export FAIRSOFT_VERSION=oct17p1
 fi
 export BUILDDIR=$2
 fi
 
 echo "CDASHUSERPATH: " $CDASHUSERPATH
 echo "SIMPATH: " $SIMPATH
+echo "FAIRROOTPATH: " $FAIRROOTPATH
 echo "SOURCEDIR: " $SOURCEDIR
 echo "BUILDDIR: " $BUILDDIR
 echo "LINUX_FLAVOUR: " $LINUX_FLAVOUR
