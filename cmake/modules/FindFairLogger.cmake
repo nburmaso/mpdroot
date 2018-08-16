cfd94e3d8caee73395fab092abc11f10ac8f991d
Konstantin Gertsenberger <soul@se51-99.jinr.ru>
2018-08-16 15:20:37 +0300

    some changes in FindFairLogger to correspond to FairRoot naming

--- cmake/modules/FindFairLogger.cmake
+++ cmake/modules/FindFairLogger.cmake
@@ -1,32 +1,34 @@
 # Find FairLogger installation
 # Once done this will define
 #  FAIRLOGGER_FOUND - system has found FairLogger installation
-#  FAIRLOGGER_INCLUDE_DIR - FairLogger include directory
-#  FAIRLOGGER_LIBRARY_DIR - FairLogger library directory
+#  FairLogger_INCDIR - FairLogger include directory
+#  FairLogger_LIBDIR - FairLogger library directory
 
-find_path(FAIRLOGGER_INCLUDE_DIR NAMES fairlogger/Logger.h PATHS
-  ${FAIRLOGGER_ROOT}/include  
+find_path(FairLogger_INCDIR NAMES fairlogger/Logger.h PATHS
+  ${FAIRLOGGER_ROOT}/include
+  $ENV{FAIRLOGGER_ROOT}/include
   ${FAIRROOTPATH}/include
   NO_DEFAULT_PATH
 )
 
-find_path(FAIRLOGGER_LIBRARY_DIR NAMES libFairLogger.so PATHS
+find_path(FairLogger_LIBDIR NAMES libFairLogger.so PATHS
   ${FAIRLOGGER_ROOT}/lib
+  $ENV{FAIRLOGGER_ROOT}/lib
   ${FAIRROOTPATH}/lib
   NO_DEFAULT_PATH
 )
 
-if (FAIRLOGGER_INCLUDE_DIR AND FAIRLOGGER_LIBRARY_DIR)
+if (FairLogger_INCDIR AND FairLogger_LIBDIR)
   set(FAIRLOGGER_FOUND TRUE)
-  SET(LD_LIBRARY_PATH ${LD_LIBRARY_PATH} ${FAIRLOGGER_LIBRARY_DIR})
+  SET(LD_LIBRARY_PATH ${LD_LIBRARY_PATH} ${FairLogger_LIBDIR})
   if (NOT FAIRLOGGER_FIND_QUIETLY)
-    message(STATUS "Looking for FairLogger... found at ${FAIRLOGGER_LIBRARY_DIR}")
+    message(STATUS "Looking for FairLogger... found at ${FairLogger_LIBDIR}")
   endif (NOT FAIRLOGGER_FIND_QUIETLY)
-else (FAIRLOGGER_INCLUDE_DIR AND FAIRLOGGER_LIBRARY_DIR)
+else (FairLogger_INCDIR AND FairLogger_LIBDIR)
   set(FAIRLOGGER_FOUND FALSE)
   if (FAIRLOGGER_FIND_REQUIRED)
     message(FATAL_ERROR "Looking for FairLogger... not found")
   else (FAIRLOGGER_FIND_REQUIRED)
     message(STATUS "Looking for FairLogger... not found")
   endif (FAIRLOGGER_FIND_REQUIRED)
-endif (FAIRLOGGER_INCLUDE_DIR AND FAIRLOGGER_LIBRARY_DIR)
+endif (FairLogger_INCDIR AND FairLogger_LIBDIR)
