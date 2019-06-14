9dcbc7891d686dbe5baea8f2241c22919a2906f8
batyuk <batyuk@vm221-59>
2019-06-14 16:23:44 +0300

    Main macro to be used with MpdFemtoPackage

--- macro/physical_analysis/femto/femtoAna.C
+++ macro/physical_analysis/femto/femtoAna.C
@@ -1,77 +1,155 @@
-#if !defined(__CINT__) && !defined(__CLING__)
-#include <iostream>
+#include <Rtypes.h>
 #include <TChain.h>
-#include <TParticle.h>
-#include <TParticlePDG.h>
-#include <TDatabasePDG.h>
-#include <fstream>
-#include <TH1.h>
+#include <TString.h>
+#include <TFile.h>
 
-using namespace std;
-#endif
+R__ADD_INCLUDE_PATH($VMCWORKDIR)
+#include "macro/mpd/mpdloadlibs.C"
+        
+/* 
+ * Main macro to do femtoscopy studies with MpdFemtoPackage ($VMCWORKDIR/physics/femto) 
+ * Input file (filename) should be given in MpdMcDst-format ($VMCWORKDIR/mpddst/mcDst)
+ * nevents is a number of events to be proccesed
+ */        
 
-void femtoAna(TString inFileDST = "/nica/mpd16/basalaev/mpddata/mpddst_",
-              TString inFileEve = "/nica/user/b/basalaev/mpdroot_vHLLE/macro/mpd/simReco/evetest_",
-              TString outFile = "_test.root", Int_t nStartEvent = 0, Int_t nEvents = 1000, Int_t nFiles = 93)
-{
-    Float_t Qinv = 0.30;
-    Int_t nKtBins = 6; // Define number of kT-bins to be used
-    Int_t dim = nKtBins + 1;
-    Float_t* KtRanges = new Float_t[dim]{0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.5};
-
-    MpdFemtoHistos* histos = new MpdFemtoHistos(Qinv, nKtBins, 3, outFile);
-    histos->SetfKtBins(nKtBins);
-    for (Int_t idx = 0; idx < dim; idx++)
-        histos->SetfKtRange(idx, KtRanges[idx]);
-    delete [] KtRanges;
-   
-    for (Int_t iFile = 0; iFile < nFiles; iFile++) {
-        // /nica/user/b/basalaev/vHLLE_mpdroot - is a path on NICA-cluster to the test data sample (vHLLE+UrQMD, 11.5 GeV/n, 1PT, MPD, geometry_stage1)
-        MpdFemto* femto = new MpdFemto(inFileDST + TString::Format("%d.root", iFile), histos);
-        // MpdFemto* femto = new MpdFemto(inFileDST + TString::Format("%d.root", iFile), inFileEve + TString::Format("%d.root", iFile), histos);
-
-        femto->SetStartEvent(nStartEvent);
-        femto->SetEvNumToRead(nEvents);
-        femto->SetSourceSize(3.); // Source size in fm;
-        femto->SetPdgCode(211); // pi+ are considered;
-        femto->SetEtaCuts(-1., 1.); // Define a range over pseudorapidity: val1 < eta < val2;
-        // femto->SetPtCuts(0.15, 1.5); // Define a range over Pt: val1 < Pt < val2;
-        femto->SetNumMixedEvents(10); // Number of events to be mixed
-        femto->SetQinv(Qinv);
-        femto->SetMagField(0.5); // Define magnitude of the mag.field
-        femto->SetRadTpc(1.0); // Define average TPC rad. where split. && merg. effects are studied
-        femto->SetMinNoHits(40);
-        femto->SetZeroSharing(kTRUE); // If true, pairs with the number of shared hits equal to 0, are put in the histo _hSharing.
-        femto->SetSplittingCut(kTRUE);
-        femto->SetSharingCut(kTRUE);
-        femto->SetQualityMax(-0.4);
-        femto->SetSharingMax(0.);
-
-        // A method to perform 1D-analysis
-        //	femto->MakeCFs_1D();
-
-        // A method to perform 3D-analysis
-        femto->MakeCFs_3D();
-
-        // A method to perform SH-analysis
-        femto->MakeCFs_SH();
-
-        // A method to perform DEtaDPhi-analysis
-        // femto->DeltaEtaDeltaPhi();
-
-        // A method to obtain Quality & Sharing cuts
-        // femto->QualityAndSharing();
-
-        // femto->EffSplitting();
-        delete femto;
+void femtoAna(TString filename = "", Int_t nevents = -1) {
+    if (filename.IsNull() || nevents == -1) {
+        cout << "Please, specify a correct input!" << endl;
+        return;
     }
+    //
+    // Monte Carlo input setup
+    //
+    MpdMcDstReader *mcDstReader = new MpdMcDstReader(filename.Data());
+    mcDstReader->Init();
+    mcDstReader->setStatus("*", 0);
+    mcDstReader->setStatus("Event", 1);
+    mcDstReader->setStatus("Particle", 1);
 
-    // Return parameters of the 1D-fit. [0] corresponds to R0, [1] -- to \lambda
-    // Double_t* fitRes1D = histos->GetFitParams1D();
-    // cout << "R0 = " << fitRes1D[0] << " Lambda = " << fitRes1D[1] << endl;
+    if (!mcDstReader->chain()) {
+        std::cout << "No chain has been found." << std::endl;
+    }
+    Long64_t events2read = mcDstReader->chain()->GetEntries();
+    std::cout << "Number of events in chain: " << events2read << std::endl;
 
-    // Return parameters of the 3D-fit. [0] corresponds to Rout, [1] -- to Rside, [2] -- to Rlong
-    histos->GetFitParams3D();
-   
-    delete histos;
+    // Correct the amount event to read
+    if (events2read >= nevents)
+        events2read = nevents;
+
+    //
+    // StHbtMaker setup
+    //
+
+    // StHbtMaker
+    MpdFemtoMaker* hbtMaker = new MpdFemtoMaker("HBT", "title");
+    std::cout << "StHbtMaker instantiated" << std::endl;
+
+    // Create manager
+    MpdFemtoManager* hbtManager = hbtMaker->hbtManager();
+
+    // Setup StHbtReader
+    MpdFemtoMcDstReader *mcHbtReader = new MpdFemtoMcDstReader(mcDstReader);
+    mcHbtReader->setRotateEventPlane(false);
+    mcHbtReader->setMagneticField(-5.); // in kilogaus = -0.5 Tesla
+
+    // Add reader to the manager
+    hbtManager->setEventReader(mcHbtReader);
+
+    // Create and set analysis
+    MpdFemtoAnalysis *hbtAnalysis = new MpdFemtoAnalysis();
+    hbtAnalysis->setVerboseMode(false);
+
+    // Create and set event cut
+    MpdFemtoBasicEventCut *eventCut = new MpdFemtoBasicEventCut();
+    hbtAnalysis->setEventCut(eventCut);
+
+    // Create and set track cut
+    MpdFemtoBasicTrackCut *trackCut = new MpdFemtoBasicTrackCut();
+    trackCut->setIsTheory(true);
+    trackCut->setPdgId(321);
+    trackCut->setEta(-1., 1.);
+    trackCut->setPt(0.15, 1.55);
+    trackCut->setMass(M_KAON_PLUS); // from femto/phys_constants.h
+
+    // Create and set cut monitor
+    MpdFemtoTrackCutMonitor *trackMoniPass = new MpdFemtoTrackCutMonitor("_moniPass_", M_KAON_PLUS);
+    MpdFemtoTrackCutMonitor *trackMoniFail = new MpdFemtoTrackCutMonitor("_moniFail_", M_KAON_PLUS);
+    trackCut->addCutMonitor(trackMoniPass, trackMoniFail);
+
+    // Set particle cuts
+    hbtAnalysis->setFirstParticleCut(trackCut);
+    hbtAnalysis->setSecondParticleCut(trackCut);
+
+    // Create and set pair cut
+    MpdFemtoBasicPairCut *pairCut = new MpdFemtoBasicPairCut();
+
+    // Create pair cut monitors
+    MpdFemtoPairCutMonitor *pairMoniPass = new MpdFemtoPairCutMonitor("_moniPass_");
+    MpdFemtoPairCutMonitor *pairMoniFail = new MpdFemtoPairCutMonitor("_moniFail_");
+    pairCut->addCutMonitor(pairMoniPass, pairMoniFail);
+
+    // Add pair cut to the analysis
+    hbtAnalysis->setPairCut(pairCut);
+
+    // Set how many events to mix
+    hbtAnalysis->setNumEventsToMix(5);
+
+    // Lednicky weight generator
+    MpdFemtoModelWeightGeneratorLednicky *hbtWeight = new MpdFemtoModelWeightGeneratorLednicky();
+    hbtWeight->setPairType(MpdFemtoModelWeightGeneratorLednicky::KaonPlusKaonPlus());
+    hbtWeight->setCoulOff();
+    hbtWeight->setQuantumOn();
+    hbtWeight->setStrongOff();
+    hbtWeight->set3BodyOff();
+
+    // Theoretical model manager
+    MpdFemtoModelManager *thModelManager = new MpdFemtoModelManager();
+    thModelManager->setWeightGenerator(hbtWeight);
+    thModelManager->createCopyHiddenInfo(true);
+
+    // Create 1D correlation function integrated over kT
+    MpdFemtoModelQinvCorrFctn *thCorrFctn = new MpdFemtoModelQinvCorrFctn("hTheorQinv", 40, 0., 0.4);
+    thCorrFctn->connectToManager(thModelManager);
+
+    // Create 3D correlation function integrated with kT binning
+    MpdFemtoModelBPLCMS3DCorrFctnKt *thCorrFctn3D = new MpdFemtoModelBPLCMS3DCorrFctnKt("hTheorBPLCMS", 80, -0.4, 0.4, 9, 0.05, 0.95);
+    thCorrFctn3D->connectToManager(thModelManager);
+
+    // Add correlation function to the analysis
+    hbtAnalysis->addCorrFctn(thCorrFctn);
+    hbtAnalysis->addCorrFctn(thCorrFctn3D);
+
+    // Add analysis to the manager
+    hbtManager->addAnalysis(hbtAnalysis);
+
+    // Initialize StHbtMaker
+    int iret = hbtMaker->init();
+
+
+    // Loop over events
+    for (int iEvent = 0; iEvent < events2read; iEvent++) {
+
+        hbtMaker->clear();
+        std::cout << "hbtMcDst -- Working on event # " << iEvent << std::endl;
+        iret = hbtMaker->make();
+
+        if (iret != 0) {
+            std::cout << "[WARNING] StHbtMaker returned status: " << iret
+                    << ". Processing will be finished" << std::endl;
+            break;
+        }
+    } // for (int iEvent=0; iEvent<events2read; iEvent++)
+
+    // Maker finish flag
+    iret = hbtMaker->Finish();
+
+    // Write histograms to the output file
+    TFile *oFile = new TFile("oHbtTest.root", "recreate");
+    trackMoniPass->writeOutHistos();
+    trackMoniFail->writeOutHistos();
+    pairMoniPass->writeOutHistos();
+    pairMoniFail->writeOutHistos();
+    thCorrFctn->writeOutHistos();
+    thCorrFctn3D->writeOutHistos();
+    oFile->Close();
 }
