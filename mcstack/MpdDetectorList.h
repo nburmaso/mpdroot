83b09d8decce45a572f860864f67f22a4a118ad9
Luis Valenzuela Cazares <luisvc3737@gmail.com>
2020-04-03 04:00:39 +0300

    Update MpdDetectorList.h

--- mcstack/MpdDetectorList.h
+++ mcstack/MpdDetectorList.h
@@ -9,6 +9,6 @@
 #ifndef MPDDETECTORLIST_H
 #define MPDDETECTORLIST_H 1
  
-enum DetectorIdMPD {kSTS, kTPC, kTOF, kETOF, kFFD, kECT, kECAL, kNDET, kCPC, kBBC, kZDC, kFSA, kSFT, kBMD, kRECOIL};
+enum DetectorIdMPD {kSTS, kTPC, kTOF, kETOF, kFFD, kECT, kECAL, kNDET, kCPC, kBBC, kZDC, kFSA, kSFT, kBMD, kMBB, kRECOIL};
 
 #endif
