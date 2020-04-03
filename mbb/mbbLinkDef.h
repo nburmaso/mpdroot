0968e85a0c70af278453f41e0658cac13b432d34
Luis Valenzuela Cazares <luisvc3737@gmail.com>
2020-04-03 04:12:06 +0300

    Upload New File

---
+++ mbb/mbbLinkDef.h
@@ -0,0 +1,15 @@
+// $Id: MbbLinkDef.h, v 1.0 2019/06/25 18:43:00  Exp $
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class MbbDetector+;
+#pragma link C++ class MbbPoint+;
+#pragma link C++ class MbbGeo+;
+#pragma link C++ class MbbGeoPar+;
+
+
+#endif
